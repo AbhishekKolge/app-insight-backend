0120013c7875495449a778e0b1a88b00a4cf6b6e
AbhishekUnthread <abhishek.k@unthread.world>
2024-01-05 05:35:45 +0530

    test user restriction enabled for delete account endpoint

--- src/middleware/test-user.js
+++ src/middleware/test-user.js
@@ -1,10 +1,11 @@
 const CustomError = require('../errors');
 
 const testUserMiddleware = (req, res, next) => {
-  //*uncomment it to restrict test user access
-  // if (req.user.testUser) {
-  //   throw new CustomError.UnauthorizedError('Test user can only read');
-  // }
+  if (req.user.testUser) {
+    throw new CustomError.UnauthorizedError(
+      "Test user can't perform this action"
+    );
+  }
   next();
 };
 
